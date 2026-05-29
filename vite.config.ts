3cc177283b819c16fd75859949a0021014cc322e
Amanuel Legesse <85198830+Skywalker416@users.noreply.github.com>
2026-05-29 14:25:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 import contentCollections from '@content-collections/vite'
 
-const config = defineConfig({
-
-  base: "/portfolio/",
+export default defineConfig({
+  base: "/Portfolio/",  // capital P to match your repo name exactly
   plugins: [
     contentCollections(),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    netlify(),
-    tanstackStart(),
     viteReact(),
   ],
 })
-
-export default config
